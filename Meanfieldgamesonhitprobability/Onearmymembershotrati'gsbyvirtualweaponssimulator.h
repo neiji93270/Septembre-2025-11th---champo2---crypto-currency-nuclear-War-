7d95e4a9456f5c4c671d3653a55856e14bf09b50
neiji93270 <nejdet.altinisik@yahoo.fr>
2025-09-15 22:11:02 +0200

    Create Onearmymembershotrati'gsbyvirtualweaponssimulator.h

---
+++ Meanfieldgamesonhitprobability/Onearmymembershotrati'gsbyvirtualweaponssimulator.h
@@ -0,0 +1,8 @@
+//being a member of only one army and stay here is the best.
+//so the virtual world is a maximum entropy
+//of risk society. the benefits of avoidin medical datas storage
+//would be to preserve pc and cloud hard disks to fail recoveri'g datas after
+//ennemies attacks. then the best thing to do would be
+//to promote animals safety. after that, os can predict by subsystems
+//recovery softwares to forbid money transfer if znimals can use devices.
+//so that here we have ro code yerkosh softwares 
